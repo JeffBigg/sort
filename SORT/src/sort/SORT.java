9ad0a669ff7589a1e6afd61b47f711c835e8e425
JeffBigg <soriaarandajefeson@gmail.com>
2023-08-24 15:44:30 -0500

    se crea la funcion partes

--- SORT/src/sort/SORT.java
+++ SORT/src/sort/SORT.java
@@ -5,11 +5,25 @@ public class SORT {
 
     public static void quicksort(int[] arr, int menor, int mayor) {
         if(menor<mayor){
-            int pivot = partition(arr, menor,mayor);
+            int pivot = partes(arr, menor,mayor);
             quicksort(arr, menor, pivot-1);
             quicksort(arr, pivot+1, mayor);
             
         }
     }
+    public static int partes(int[] arr, int menor, int mayor){
+        int pivot = arr[mayor];
+        int i = menor -1;
+        
+        for (int j = menor; j<mayor; j++){
+            if (arr[j]<pivot){
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,i+1,mayor);
+        return i + 1;
+    }
+    
     
 }
